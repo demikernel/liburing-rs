ecc43bfa6864ba59434f7b6ed8a231026efdda1e
Pedro Henrique Penna <ppenna@microsoft.com>
2022-08-01 11:49:44 -0300

    [liburing] Enhancement: Renaming Variables

--- inlined.c
+++ inlined.c
@@ -19,9 +19,9 @@ void io_uring_prep_send_(struct io_uring_sqe *sqe, int sockfd, const void *buf, 
     io_uring_prep_send(sqe, sockfd, buf, len, flags);
 }
 
-void io_uring_prep_sendmsg_(struct io_uring_sqe *sqe, int fd, const struct msghdr *msg, unsigned flags)
+void io_uring_prep_sendmsg_(struct io_uring_sqe *sqe, int sockfd, const struct msghdr *msg, unsigned flags)
 {
-    io_uring_prep_sendmsg(sqe, fd, msg, flags);
+    io_uring_prep_sendmsg(sqe, sockfd, msg, flags);
 }
 
 void io_uring_prep_sendto_(struct io_uring_sqe *sqe, int sockfd, const void *buf, size_t len, int flags,
